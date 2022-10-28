304561897604973485eca25ee5cbc8b777862296
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-10-28 15:35:04 +0700

    update

--- application/modules/stok/views/index.php
+++ application/modules/stok/views/index.php
@@ -188,6 +188,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/back/theme/vendor/daterangepicker/daterangepicker.css">
     
     <script type="text/javascript">
+    $('input.numberformat').number( true, 0,',','.' );
 	$('#filter_date_stok').daterangepicker({
         autoUpdateInput: false,
         locale: {
